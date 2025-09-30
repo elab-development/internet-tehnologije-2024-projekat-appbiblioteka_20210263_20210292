c2d008fbee92cdea4cbf60871eac3814a08b9a90
Nevena <nevena.mitrovic.bgd@gmail.com>
2025-09-30 19:12:20 +0200

    about page

---
+++ biblioteka-react/src/pages/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from "../components/Header";
+
+const About = () => {
+    return (
+        <div>
+            <Header title="About La Biblioteka"/>
+            <div className="content">
+                <p>Welcome to La Biblioteka, your go-to library management system. Our platform is designed to make it easy for you to manage your book collection, track lends, and register new members.</p>
+                <p>With La Biblioteka, you can:</p>
+                <ul>
+                    <li>View and manage your book collection</li>
+                    <li>Track who has borrowed which books</li>
+                    <li>Register new members quickly and easily</li>
+                    <li>Access your profile and update your information</li>
+                </ul>
+                <p>We are committed to providing a seamless experience for library administrators and members alike. Thank you for choosing La Biblioteka!</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default About;
