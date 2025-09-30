3fd1c375039664c7c34af29b8187a4650d7a2a20
Nevena <nevena.mitrovic.bgd@gmail.com>
2025-09-30 18:56:35 +0200

    komponenta za futer

---
+++ biblioteka-react/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <span>
+                &copy; {new Date().getFullYear()} La Biblioteka. All rights reserved.
+            </span>
+        </footer>
+    );
+};
+
+export default Footer;
