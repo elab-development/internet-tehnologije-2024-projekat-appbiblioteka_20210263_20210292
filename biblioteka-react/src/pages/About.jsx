7b404ade0ead867c933c350e706d6a95ad674c33
Nevena <nevena.mitrovic.bgd@gmail.com>
2025-10-01 17:43:40 +0200

    uredjivanje about stranice

--- biblioteka-react/src/pages/About.jsx
+++ biblioteka-react/src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "../components/Header";
+import logo from '../images/citat.png';
 
 const About = () => {
     return (
@@ -15,7 +16,19 @@ const About = () => {
                     <li>Access your profile and update your information</li>
                 </ul>
                 <p>We are committed to providing a seamless experience for library administrators and members alike. Thank you for choosing La Biblioteka!</p>
-            </div>
+
+        <div className="flex justify-center items-center mt-8">
+             <a href="/books">
+                    <img
+                        src={logo}
+                        alt="Citat"
+                      style={{ width: '1000px', height: 'auto' }}
+                        className="rounded-2xl shadow-lg"
+                    />
+                 </a>
+                </div>
+
+         </div>
 
         </div>
     );
